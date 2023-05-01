f3bd2ef521146c411c0787c17fa6a22e204f871e
Tran Minh Hai <hai@bio-rithm.com>
2023-05-01 15:34:25 +0700

    test

--- app/app.py
+++ app/app.py
@@ -4,7 +4,11 @@
 from flask import Flask, render_template
 import json
 
-app = Flask(__name__)
+# app = Flask(__name__)
+app = Flask("my app")
+
+app.jinja_env.auto_reload = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # get book list should be even number
 with open("./static/book.json", "rb") as file:
@@ -16,5 +20,14 @@ def index():
     return render_template("index.html", books=books, nrow=len(books))
 
 
+def gen_static_web():
+    with app.app_context():
+        rendered = render_template(
+            "index.html", books=books, nrow=len(books)
+        )
+        print(rendered)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8081, debug=True)
+# gen_static_web()
